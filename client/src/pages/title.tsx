210a0ac6e9dc8818e5e721309a3ab0a130cd58cb
builenganha <bui.n.aa@m.titech.ac.jp>
2023-05-22 23:57:51 +0900

    added description and logo

--- client/src/pages/title.tsx
+++ client/src/pages/title.tsx
@@ -1,10 +1,13 @@
+import logo from "../assets/mindgraphLogo.jpg"
+
 const Title = () => {
+  //TODO コンポーネントを置き換える
     return (
       <div>
         <h1>MIND GRAPH</h1>
         <p>MINNA NO NOU WO KASHIKASURU</p>
 
-        
+        <img src={logo} className="logo mind graph" alt="Mind graph logo" />
 
         <form>
           <div className="input-group">
