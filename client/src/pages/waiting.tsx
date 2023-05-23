12947486d7f3a23f63d0c64eec2c29d26b83dc6e
cyan5 <r.vanis2000@gmail.com>
2023-05-23 17:20:06 +0900

    待機画面に主題を入力できる

--- client/src/pages/waiting.tsx
+++ client/src/pages/waiting.tsx
@@ -1,11 +1,32 @@
-const Waiting = () => {
-    return (
-      <div>
+// import React from 'react';
+import React from 'react';
+
+const Waiting: FC = () => {
+  return (
+    <div>
+      <hgroup>
         <h1>Waiting</h1>
-        <p>This is the waiting page</p>
-      </div>
-    )
-  }
-  
-  export default Waiting
+      </hgroup>
+
+      <form>
+        <div>
+          <p>input theme</p>
+        </div>
+        <div>
+          {/* <textarea></textarea> */}
+          {/* <TextareaComp name="TextareaComp" value={state.comment} /> */}
+        </div>
+        <div>
+          <input type="text" required minlength="1" maxlength="32" size="32"/>
+          {/* <InputComp name="InputComp" value={state.comment} /> */}
+        </div>
+        <div>
+          <button>send</button>
+        </div>
+      </form>
+    </div>   
+  )
+}
+
+export default Waiting
   
