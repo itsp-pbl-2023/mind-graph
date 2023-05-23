9ba10bcf16a4551408be5adf36fd8407cc7ea0c9
builenganha <bui.n.aa@m.titech.ac.jp>
2023-05-23 17:34:08 +0900

    test context

--- client/src/pages/title.tsx
+++ client/src/pages/title.tsx
@@ -3,12 +3,9 @@ import logo from "../assets/mindgraphLogo.jpg"
 import React, {ChangeEvent, useState} from "react"
 import Button from "../components/button"
 
-/*export*/ const [userName, setUserName] = useState("");
-//export const nameContext = React.createContext("")
-
-
-
 const Title = () => {
+  const [userName, setUserName] = useState("");
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setUserName(e.target.value);
   }
@@ -26,12 +23,13 @@ const Title = () => {
 
         <img src={logo} alt="Mind graph logo" />
 
+        <p>名前を入力</p>
         <form>
           <div className="input-group">
-            <label htmlFor="name">Name</label>
+            <label htmlFor="name">名前</label>
             <input type="text" id="name" onChange={handleChange} value = {userName}/>
           </div>
-          <Button text="submit" onClick={navigateToWaiting}/>
+          <Button text="送信" onClick={navigateToWaiting}/>
         </form>
       </div> 
     )
