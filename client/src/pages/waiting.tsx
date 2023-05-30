598687ea4e1a4c4579984e47562205414e00e732
builenganha <bui.n.aa@m.titech.ac.jp>
2023-05-30 15:45:21 +0900

    adde theme input on waiting and game

--- client/src/pages/waiting.tsx
+++ client/src/pages/waiting.tsx
@@ -1,19 +1,28 @@
 import { useNavigate } from "react-router-dom"
 import { useSetTheme } from "../lib/hooks/theme"
+import {useState} from "react"
 
 const Waiting = () => {
   const setTheme = useSetTheme()
   const navigate = useNavigate();
+  const [theme, setInputTheme] = useState("");
 
   const testSetTheme = () => {
-    setTheme('test theme')
+    setTheme(theme);
     navigate("/game")
   }
 
   return (
       <div>
         <h1>Waiting</h1>
-        <button onClick={testSetTheme}></button>
+        <form>
+          <div>
+            <label>Enter theme:</label>
+            <input type="text" value={theme} onChange={(e) => setInputTheme(e.target.value)}/>
+            <button onClick={testSetTheme}></button>
+          </div>
+        </form>
+        
         <p>This is the waiting page</p>
       </div>
     )
