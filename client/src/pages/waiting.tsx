59218cc5af6902b4bf5b7d58ed22c5b8535bf099
cyan5 <r.vanis2000@gmail.com>
2023-06-13 13:19:45 +0900

    誰かがテーマを入力すると参加者全員がゲーム画面に遷移する ボタンにSend文字を追加 ボタン位置を入力欄の下に移動

--- client/src/pages/waiting.tsx
+++ client/src/pages/waiting.tsx
@@ -1,32 +1,48 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useSetTheme } from "../lib/hooks/theme"
+import { useOnEvent } from "../lib/hooks/stream"
+import { client } from "../lib/client"
+import { useName } from "../lib/hooks/name"
+import Button from "../components/button"
 
 const Waiting = () => {
-  const setTheme = useSetTheme()
+  const name = useName();
+  const setTheme = useSetTheme();
   const navigate = useNavigate();
   
   const [themeText, setThemeText] = useState("");
   const testSetTheme = () => {
-    setTheme(themeText)
-    navigate("/game")
+    client.setTheme({
+      theme: themeText, 
+      senderId: name, 
+    })
   }
+
+  useOnEvent(useCallback((event) => {
+    if(event.event.case === "themeConfirmed"){
+      setTheme(event.event.value.theme)
+      navigate("/game")
+    }
+  }, []))
 
   return (
     <div>
       <div>
         <h1>Waiting</h1>
-        <button onClick={testSetTheme}></button>
         <p>This is the waiting page</p>
       </div>
-
       <div>
         <input
           value={themeText}  // 入力を格納する変数
           onChange={(event) => setThemeText(event.target.value)}
-        />
+          />
       </div>
-    </div>   
+      <div>
+        {/* <button onClick={testSetTheme}>send</button> */}
+        <Button text="Send" onClick={testSetTheme}/>
+      </div>
+    </div>
   )
 }
 
