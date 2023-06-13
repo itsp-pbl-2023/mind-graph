49ad935ad5cd682c1a132e469572ae296e0d481d
builenganha <bui.n.aa@m.titech.ac.jp>
2023-06-13 14:50:13 +0900

    added button in voting

--- client/src/pages/voting.tsx
+++ client/src/pages/voting.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react"
 import { ThemeDisplay } from "../components/common/ThemeDisplay"
 
+
 const Voting = () => {
+  const [active, setActive] = useState(false);
+  const onClickHandler = () => {
+    setActive(current => !current);
+  }
+
     return (
       <div>
         <h1>Voting</h1>
         <ThemeDisplay />
         <p>This is the voting page</p>
+        <button onClick={onClickHandler}
+        style = {{backgroundColor: active? 'green':'white'}}></button>
       </div>
+      
     )
   }
   
