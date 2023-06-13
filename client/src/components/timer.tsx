f93241880cb86fe06ff4a5d68f232fcc041d3595
Yuzuki Sakashita <sakashita.y.aa@m.titech.ac.jp>
2023-06-13 15:31:12 +0900

    関数の切り出し

--- client/src/components/timer.tsx
+++ client/src/components/timer.tsx
@@ -1,36 +1,35 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
+const msec2second = (msec: number) :number => {
+    return Math.floor(msec/1000)
+}
+
+// 現在時刻との差分を取ることで残り秒数(msec)を計算
+const calcRemain = (expire: Date) : number => {
+    const now = new Date()
+    const remain = expire.getTime() - now.getTime()
+
+    return remain
+}
+
 const Timer = ({expire} : {expire: Date}) => {
     const [remain, setRemain] = useState(100)
-
     // 時間切れ
     const navigate = useNavigate()
     const onTimeUp = () => {
         navigate("/voting")
     }
 
-    // 現在時刻との差分を取ることで残り秒数(msec)を計算
-    const updateRemain = () : number => {
-        const now = new Date()
-        const remain = expire.getTime() - now.getTime()
-
-        return remain
-    }
-
-    const msec2second = (msec: number) :number => {
-        return Math.floor(msec/1000)
-    }
-
     // mount時処理
     useEffect(() => {
         // 最初のレンダリング
-        setRemain(updateRemain)
+        setRemain(() => calcRemain(expire))
         
         const render_remain = () : void => {
             window.requestAnimationFrame(() => {
                 setRemain(() => {
-                    return updateRemain()
+                    return calcRemain(expire)
                 })
                 requestAnimationFrame(render_remain)
             })
@@ -45,6 +44,7 @@ const Timer = ({expire} : {expire: Date}) => {
             console.log("Finish!")
             onTimeUp()
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [remain])
 
     return (
