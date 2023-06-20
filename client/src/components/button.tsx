ccfe68339855eb9313fc9351bb07e788ebc47bf5
builenganha <bui.n.aa@m.titech.ac.jp>
2023-06-20 14:20:21 +0900

    button style

--- client/src/components/button.tsx
+++ client/src/components/button.tsx
@@ -1,6 +1,44 @@
 declare type onClickHandler = () => void
 
-const Button = ({ text, onClick }: { text: string, onClick: onClickHandler }) => {
+interface Props {
+  border: string;
+  color: string;
+  children?: React.ReactNode;
+  height: string;
+  onClick: () => void;
+  radius: string
+  width: string;
+}
+
+// const Button: React.FC<Props> = ({ 
+//   border,
+//   color,
+//   children,
+//   height,
+//   onClick, 
+//   radius,
+//   width
+// }) => { 
+// return (
+//   <button 
+//     onClick={onClick}
+//     style={{
+//        backgroundColor: color,
+//        border,
+//        borderRadius: radius,
+//        height,
+//        width
+//     }}
+//   >
+//   {children}
+//   </button>
+// );
+// }
+
+const Button = (
+    { text, onClick }: { text: string, onClick: onClickHandler }
+    
+    ) => {
 
     const onClickHander = () => {
         onClick();
