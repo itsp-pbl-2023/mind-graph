b68e7f5e3fe30f4b82eaf75449685a590c85255b
builenganha <bui.n.aa@m.titech.ac.jp>
2023-06-27 16:00:26 +0900

    paradesign

--- client/src/components/explainText.tsx
+++ client/src/components/explainText.tsx
@@ -1,14 +1,28 @@
-const explainText = ({ elements }: { elements: string[] }) => {
+import styled from "styled-components"
+
+const ExplainTextStyle = styled.ul`
+  display: inline-block;
+  color: black;
+  background-color: white;
+  border: 4px solid #B2D8F5;
+  text-align: center;
+  width: 240px;
+  margin: 4px;
+  padding: 60px;
+
+`
+
+const ExplainText = ({ elements }: { elements: string[] }) => {
 
   const elementsList = elements.map(element => <li>{element}</li>)
 
   return (
     <div>
-      <ul>
+      <ExplainTextStyle>
         { elementsList }
-      </ul>
+      </ExplainTextStyle>
     </div>
   )
 }
 
-export default explainText
+export default ExplainText
