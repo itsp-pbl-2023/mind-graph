f964bfcba54ce7564d149066da624ef4d261f2d6
cyan5 <r.vanis2000@gmail.com>
2023-06-27 17:32:22 +0900

    show word

--- client/src/pages/result.tsx
+++ client/src/pages/result.tsx
@@ -2,8 +2,6 @@ import Button from '../components/button'
 import { useNavigate } from 'react-router-dom'
 import { ThemeDisplay } from '../components/common/ThemeDisplay'
 import { getResult } from '../lib/state/result.ts'
-import ShowYourScore from '../components/showYourScore.tsx'
-import ShowMVP from '../components/showMVP.tsx'
 import ShowWord from '../components/showWord.tsx'
 import { useSetName } from '../lib/hooks/name.ts'
 
@@ -23,8 +21,6 @@ const Result = () => {
       <p>This is the result page</p>
 
       <ShowWord word={getResult()?.chosenNodeID} />
-      <ShowMVP userID={getResult()?.mvpUserID} />
-      <ShowYourScore score={getResult()?.myScore} />
 
       <Button text='タイトルに戻る' onClick={returnToTitle} />
     </div>
