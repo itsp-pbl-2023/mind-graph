82616d579c401e10e6a7392b2b8048337a7f995f
motoki317 <motoki317@gmail.com>
2023-07-04 14:51:35 +0900

    Result layout

--- client/src/components/userlistWaiting.tsx
+++ client/src/components/userlistWaiting.tsx
@@ -2,8 +2,9 @@ import { useUsers } from '../lib/hooks/users';
 import styled from "styled-components"
 
 const UserListStyle  = styled.div`
+  box-sizing: border-box;
+  width: 100%;
   padding:3em;
-  background-color: var(--background-color);
   border: 6px solid;
   border-color: #30A6FF;
   border-radius: 45px;
@@ -13,6 +14,12 @@ const UserListTitleStyle = styled.h1`
   font-size: 32px;
   font-family: 'Noto Sans JP';
 `
+
+const StyledList = styled.ul`
+  list-style-position: inside;
+  padding: 0;
+`
+
 const ListStyle = styled.li`
   font-family: 'Noto Sans JP';
 `
@@ -23,11 +30,11 @@ const UserList= () => {
   return (
     <UserListStyle>
       <UserListTitleStyle>参加者一覧 {users.length}人</UserListTitleStyle>
-        <ul>
-          {users.map( item =>
-            <ListStyle key={item.id}>{item.name}</ListStyle>
-          )}
-        </ul>
+      <StyledList>
+        {users.map( item =>
+          <ListStyle key={item.id}>{item.name}</ListStyle>
+        )}
+      </StyledList>
     </UserListStyle>
   )
 };
